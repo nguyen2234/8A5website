bfcb9d49b07b8db481ee736fd5d592e2c5b49271
nguyen2234 <93630179+nguyen2234@users.noreply.github.com>
2022-05-12 20:26:11 +0700

    v0.2.1

---
+++ js/all.js
@@ -0,0 +1,5 @@
+if (window.innerWidth <= 500) {
+    var rp = document.querySelector('.container')
+    var inw = innerWidth - 10 + ";"
+    rp.style = "border: 1px solid red !important; height: 800px !important;"
+}
