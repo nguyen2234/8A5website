56fbb46041cc79da9d6122769df13753cb5d6eb8
nguyen2234 <93630179+nguyen2234@users.noreply.github.com>
2022-06-29 19:24:22 +0700

    Update khac.js

--- khac.js
+++ khac.js
@@ -5,7 +5,7 @@ document.querySelector('head').innerHTML = ' <meta charset="UTF-8">\n' +
     '    <link rel="stylesheet" href="CSS/style.css">\n' +
     '    <title>8A5\'s website - Khác</title>\n' +
     '    <link rel="icon" href="img/8a5logo.png">\n' +
-    '    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/nguyen2234/8A5website/CSS/style.css">\n' +
+    '    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/nguyen2234/8A5website@main/CSS/style.css">\n' +
     '    <meta name="description" content="Trang khác của 8A5\'s website, chứa một số thông tin bên lề khác">' +
     '   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">'
 
